ab1a150dc6463beee8579c584c5a21bd9f122d5e
Brian Smith <brian.smith3@se.com>
2023-10-04 11:43:30 -0400

    Added images

--- Webpage/main.js
+++ Webpage/main.js
@@ -3,20 +3,25 @@ var isSidebarOpen = 0
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openSidebar() {
     var mySidebar = document.getElementById("mySidebar")
+    var mainSection = document.getElementById("mainSection")
+    var myButton = document.getElementById("toggleSidebarButton")
+
     mySidebar.style.display = "block"
     mySidebar.style.width = "25%"
-    document.getElementById("mainSection").style.width = "75%"
-    document.getElementById("toggleSidebarButton").value="Close sidebar";
+    mainSection.style.width = "75%"
+    myButton.innerHTML = '<img src="../img/RightArrow.png"/>'
 
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeSidebar() {
     var mySidebar = document.getElementById("mySidebar")
+    var mainSection = document.getElementById("mainSection")
+    var myButton = document.getElementById("toggleSidebarButton")
     mySidebar.style.display = "none"
     mySidebar.style.width = "0%"
-    document.getElementById("mainSection").style.width = "100%"
-    document.getElementById("toggleSidebarButton").value="Open sidebar";
+    mainSection.style.width = "100%"
+    myButton.innerHTML = '<img src="../img/LeftArrow.png"/>'
   }
 
   function toggleSidebar(){
@@ -24,5 +29,5 @@ function openSidebar() {
         closeSidebar()
     else
         openSidebar()
-        isSidebarOpen = !isSidebarOpen
+    isSidebarOpen = !isSidebarOpen
   }
