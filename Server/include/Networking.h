503aa869d7e9e3e42b29b23898f017e1fdf511f7
Brian Smith <brian.smith3@se.com>
2023-10-06 10:13:51 -0400

    Added the ability to receive http messages from a client

--- Server/include/Networking.h
+++ Server/include/Networking.h
@@ -1,12 +1,48 @@
+#include <string>
+
+/* \page Networking
+ *
+ * Examples of networking protocls are HTTP, TCP-IP, UDP
+ * - HTTP (Hypertext transfer protocol)
+ * - TCP (Transmission control protocol)
+ * - UDP (User datagram protocol)
+ * 
+ * Web browsers do not support communication with TCP-IP or UDP because
+ * of security issues.
+ * 
+ * HTTP is the main protocol for Web browsers.
+ * - Javascript can send HTTP requests using
+ *   - AJAX
+ *   - jQuery
+ *   - fetch
+ *   - etc...
+ * 
+ * For this application, I am using jQuery. The jQuery I use looks like this:
+ *   fetch('http://192.168.1.11:47808') 
+ *     .then(function(response) { 
+ *       console.log("Got it");
+ *     }, function(e) {
+ *       console.log("Error");
+ *   })
+ * 
+ * The jQuery expects the response to be similar to this:
+ *   char htmlResponse[512] = "HTTP/1.1 200 OK";
+ */
 class Networking
 {
   public:
-    Networking() = default;
+    Networking() : m_ipAddress("192.168.1.11"){};
 
     ~Networking() = default;
 
     void createConnection();
 
+    constexpr static const unsigned short DEFAULT_BUFLEN = 512;
+    constexpr static const unsigned short DEFAULT_PORT = 47808;
+
+
   private:
-    void AcceptThread();
+    std::string m_ipAddress;
+    int udpThread();
+    int tcpThread();
 };
--- Server/src/Networking.cpp
+++ Server/src/Networking.cpp
@@ -2,27 +2,216 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment (lib, "Ws2_32.lib")
 
 void Networking::createConnection()
 {
     std::cout << "createConnection()\n";
 
-    auto lambda=[]()
-    {
-        int i = 0;
-        while (i++ < 10)
-        {
-            std::cout << "Waiting\n";
-            std::this_thread::sleep_for(std::chrono::seconds(3));
-        }
-    };
-
-    std::jthread networkingThread(lambda);
+    std::jthread networkingThread(&Networking::tcpThread, this);
 
     std::cout << "createConnection finished\n";
 }
 
-void Networking::AcceptThread()
+int Networking::udpThread()
 {
+    sockaddr_in server, client;
 
+    // initialize winsock
+    WSADATA wsa;
+    std::cout << "Initializing Winsock\n";
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        std::cout << "Failed. Error Code: " << WSAGetLastError() << "\n";
+        return 1;
+    }
+
+    // create a socket
+    std::cout << "Create a socket.\n";
+    SOCKET server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+    {
+        std::cout << "Could not create socket: " << WSAGetLastError() << "\n";
+    }
+
+    // prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(m_ipAddress.c_str());
+    server.sin_port = htons(DEFAULT_PORT);
+
+    char addressBuf[512];
+    inet_ntop( AF_INET, &server.sin_addr, addressBuf, sizeof(addressBuf));
+    std::cout << "IP Address: " << addressBuf << ":" << DEFAULT_PORT << "\n";
+
+    // bind
+    std::cout << "Binding\n";
+    if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
+    {
+        std::cout << "Bind failed with error code: %d" << WSAGetLastError() << "\n";
+        return 1;
+    }
+
+    while (true)
+    {
+        std::cout << "Waiting for data...\n";
+        char message[DEFAULT_BUFLEN] = {};
+
+        // try to receive some data, this is a blocking call
+        int message_len;
+        int slen = sizeof(sockaddr_in);
+        if (message_len = recvfrom(server_socket, message, DEFAULT_BUFLEN, 0, (sockaddr*)&client, &slen) == SOCKET_ERROR)
+        {
+            std::cout << "recvfrom() failed with error code: %d" << WSAGetLastError() << "\n";
+            return 1;
+        }
+
+        // print details of the client/peer and the data received
+        std::cout << "Received packet from " << inet_ntoa(client.sin_addr) << ":" << ntohs(client.sin_port) << "\n";
+        std::cout << "Data: " << message << "\n";
+
+        // reply the client with the same data
+        if (sendto(server_socket, message, strlen(message), 0, (sockaddr*)&client, sizeof(sockaddr_in)) == SOCKET_ERROR)
+        {
+            std::cout << "sendto() failed with error code: " << WSAGetLastError() << "\n";
+            return 1;
+        }
+    }
+
+    closesocket(server_socket);
+    WSACleanup();
+}
+
+int Networking::tcpThread()
+{
+    WSADATA wsaData;
+    int iResult = 0;
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    struct addrinfo *result = NULL;
+    struct addrinfo hints;
+
+    int iSendResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+
+    std::cout << "Initialize Winsock:\n";
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        std::cout << "WSAStartup failed with error: " << iResult << "\n";
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Resolve the server address and port
+    std::cout << "Resolve the server address and port:\n";
+    iResult = getaddrinfo(NULL, std::to_string(DEFAULT_PORT).c_str(), &hints, &result);
+    if ( iResult != 0 ) {
+        std::cout << "getaddrinfo failed with error: " << iResult << "\n";
+        WSACleanup();
+        return 1;
+    }
+
+    // Create a SOCKET for the server to listen for client connections.
+    std::cout << "Create a SOCKET for the server to listen for client connections:\n";
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        std::cout << "socket failed with error: " << WSAGetLastError() << "\n";
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+    struct sockaddr_in address;
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = inet_addr(m_ipAddress.c_str());
+    address.sin_port = htons(DEFAULT_PORT); 
+
+    // Setup the TCP listening socket
+    std::cout << "Setup the TCP listening socket:\n";
+    iResult = bind( ListenSocket, (struct sockaddr*)&address, sizeof(address));
+    //iResult = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        std::cout << "bind failed with error: " << WSAGetLastError() << "\n";
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(result);
+
+    std::cout << "Listen:\n";
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        std::cout << "listen failed with error: " << WSAGetLastError() << "\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Accept a client socket
+    std::cout << "Accept a client socket:\n";
+    ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        std::cout << "accept failed with error: " << WSAGetLastError() << "\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // No longer need server socket
+    closesocket(ListenSocket);
+
+    // Receive until the peer shuts down the connection
+    do {
+
+        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            std::cout << "Bytes received: " << iResult << "\n";
+            std::cout << "Data received: " << recvbuf << "\n";
+
+            char htmlResponse[512] = "HTTP/1.1 200 OK";
+
+            iSendResult = send( ClientSocket, htmlResponse, sizeof(htmlResponse), 0 );
+            if (iSendResult == SOCKET_ERROR) {
+                std::cout << "send failed with error: " << WSAGetLastError() << "\n";
+                closesocket(ClientSocket);
+                WSACleanup();
+                return 1;
+            }
+            std::cout << "Bytes sent: " << iSendResult << "\n";
+        }
+        else if (iResult == 0)
+            std::cout << "Connection closing:\n";
+        else  {
+            std::cout << "recv failed with error: " << WSAGetLastError() << "\n";
+            closesocket(ClientSocket);
+            WSACleanup();
+            return 1;
+        }
+
+    } while (iResult > 0);
+
+    // shutdown the connection since we're done
+    iResult = shutdown(ClientSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        std::cout << "Shutdown failed with error: " << WSAGetLastError() << "\n";
+        closesocket(ClientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // cleanup
+    closesocket(ClientSocket);
+    WSACleanup();
+    return 0;
 }
