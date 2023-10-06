bda0c15e4569a19156beeb2f5cdeace9352f0e93
Brian Smith <brian.smith3@se.com>
2023-10-06 10:22:52 -0400

    Missed a file

--- Webpage/javascript/networking.js
+++ Webpage/javascript/networking.js
@@ -1,5 +1,12 @@
- 
-  function connect() {
-    console.log("Connect")
-  }
+
+function connect() {
+  console.log("Connect")
+
+  fetch('http://192.168.1.11:47808') 
+    .then(function(response) { 
+        console.log("Got it");
+    }, function(e) {
+        console.log("Error");
+    })
+}
   
